4e7d8688fc55b827a414f6c01037d5aeca90f9e4
Yehorova Olga <kelmore400@gmail.com>
2022-04-27 19:44:25 +0300

    fixing tabulation

--- src/homework/ErrorPage.js
+++ src/homework/ErrorPage.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
 export class ErrorPage extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = { hasError: false }
-    }
+  constructor(props){
+    super(props);
+    this.state = { hasError: false }
+  }
     static getDerivedStateFromError(error){
-        return { hasError: true}
-    }
+      return { hasError: true}
+  }
     componentDidCatch(error, errorInfo){
-        console.log(error, errorInfo);
-    }
+      console.log(error, errorInfo);
+  }
 
-    render(){
-        if(this.state.hasError) {
-            return <h3>Something went wrong 2</h3>
-        }
-        return <div>
-            {this.props.children}
-        </div>
-        }
+  render(){
+    if(this.state.hasError) {
+      return <h3>Something went wrong 2</h3>
+    }
+      return <div>
+        {this.props.children}
+      </div>
+  }
 }
