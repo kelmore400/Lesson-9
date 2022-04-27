1873ab490e268330d52045e73cbc2223b142e4fa
Yehorova Olga <kelmore400@gmail.com>
2022-04-27 19:45:34 +0300

    fixing tabulation

--- src/homework/Homework.js
+++ src/homework/Homework.js
@@ -4,16 +4,16 @@ import { ErrorBoundary } from './ErrorBoundary';
 import { Task1 } from './Task1';
 
 export class Homework extends React.Component {
-    render(){
-        return (
-            <div className={styles.container}>
-            <h1>Homework</h1>
-            <ErrorBoundary>
-                <Task1 />
-            </ ErrorBoundary>
-            <h2 className={styles.errorpage}>Error Page</h2>
-            <p className={styles.errorpage}>Проверить работу этого компонента можно, если написать в SupportForm вместо userName - Путин Хуйло </p>
-            </div>
-        )
-    }
+  render(){
+    return (
+      <div className={styles.container}>
+      <h1>Homework</h1>
+      <ErrorBoundary>
+        <Task1 />
+      </ ErrorBoundary>
+      <h2 className={styles.errorpage}>Error Page</h2>
+      <p className={styles.errorpage}>Проверить работу этого компонента можно, если написать в SupportForm вместо userName - Путин Хуйло </p>
+      </div>
+    )
+  }
 }
